b3c0c17767c5c968a35fb13ed4d1f3e1d3490c9e
Corwin Smith <cssmittys@gmail.com>
2022-11-21 21:21:31 -0700

    change BreadcrumbLink to NextLink

--- src/components/docs/Breadcrumbs.tsx
+++ src/components/docs/Breadcrumbs.tsx
@@ -1,27 +1,25 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
+import { Breadcrumb, BreadcrumbItem, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
 import { FC } from 'react';
 
 interface Props {
-  router: any
+  router: any;
 }
 
 export const Breadcrumbs: FC<Props> = ({ router }) => {
-  let pathSplit = router.asPath.split('/')
-  pathSplit = pathSplit.splice(1,pathSplit.length)
+  let pathSplit = router.asPath.split('/');
+  pathSplit = pathSplit.splice(1, pathSplit.length);
 
   return (
     <Breadcrumb mb={10}>
       {pathSplit.map((path: string, idx: number) => {
         return (
           <BreadcrumbItem key={path}>
-            <BreadcrumbLink
-              color={ idx+1 === pathSplit.length ? 'body' : 'primary' }
-              href={`/${pathSplit.slice(0,idx+1).join('/')}`}
-            >
-              {path}
-            </BreadcrumbLink>
+            <NextLink href={`/${pathSplit.slice(0, idx + 1).join('/')}`} passHref>
+              <Link color={idx + 1 === pathSplit.length ? 'body' : 'primary'}>{path}</Link>
+            </NextLink>
           </BreadcrumbItem>
-        )
+        );
       })}
     </Breadcrumb>
   );
