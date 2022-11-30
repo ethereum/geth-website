3c5161b92bd7342d8de009a10828ce5fb6ca0cac
Corwin Smith <cssmittys@gmail.com>
2022-11-29 22:57:35 -0700

    add lastModified date

--- src/utils/getParsedDate.ts
+++ src/utils/getParsedDate.ts
@@ -1,12 +1,10 @@
-export const getParsedDate = (date: string) => {
-  const dateOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    timeZone: 'UTC'
-  } as const;
+export const getParsedDate = (date: string | Date, dateOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: '2-digit',
+  minute: '2-digit',
+  timeZone: 'UTC'} as const) => {
 
   return new Date(date).toLocaleDateString('en', dateOptions);
 };
