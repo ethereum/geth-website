6d43a93357407178000bdff266dda1d61b45c73d
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-11-30 00:40:03 -0300

    chore: update types

--- src/types.ts
+++ src/types.ts
@@ -34,3 +34,16 @@ export interface ReleaseParams {
   blobsList: string[];
   isStableRelease: boolean;
 }
+
+export interface OpenPGPSignaturesData {
+  'build server': string;
+  'unique id': string;
+  'openpgp key': {
+    label: string;
+    url: string;
+  };
+  fingerprint: string;
+}
+
+export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+export type XOR<T, U> = T | U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
