be6c6246e868131a175710dda60077c3954af20a
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-12-01 21:17:06 -0800

    add bold for active docs link [Fixes #74]

--- src/components/UI/docs/LinkList.tsx
+++ src/components/UI/docs/LinkList.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Link, Stack, Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 
 import { NavLink } from '../../../types';
 
@@ -8,28 +9,34 @@ interface LinksListProps {
   links: NavLink[];
 }
 
-export const LinksList: FC<LinksListProps> = ({ links }) => (
-  <Stack px={4}>
-    {links.map(({ id, to, items }) => {
-      return to ? (
-        <Stack key={id}>
-          <NextLink href={to} passHref key={id}>
-            <Link>
-              <Text textStyle='docs-nav-links' color={items ? 'primary' : 'body'}>
-                {id}
-              </Text>
-            </Link>
-          </NextLink>
-          {items && <LinksList links={items} />}
-        </Stack>
-      ) : (
-        <Stack key={id}>
-          <Text textStyle='docs-nav-links' color={items ? 'primary' : 'body'}>
-            {id}
-          </Text>
-          {items && <LinksList links={items} />}
-        </Stack>
-      );
-    })}
-  </Stack>
-);
+export const LinksList: FC<LinksListProps> = ({ links }) => {
+  const router = useRouter();
+  const { slug } = router.query;
+  return (
+    <Stack px={4}>
+      {links.map(({ id, to, items }) => {
+        const split = to?.split('/')
+        const isActive = slug && split && split[split.length - 1] === slug[slug.length - 1];
+        return to ? (
+          <Stack key={id}>
+            <NextLink href={to} passHref key={id}>
+              <Link>
+                <Text textStyle='docs-nav-links' color={items ? 'primary' : 'body'} fontWeight={isActive ? 700 : 'unset'}>
+                  {id}
+                </Text>
+              </Link>
+            </NextLink>
+            {items && <LinksList links={items} />}
+          </Stack>
+        ) : (
+          <Stack key={id}>
+            <Text textStyle='docs-nav-links' color={items ? 'primary' : 'body'}>
+              {id}
+            </Text>
+            {items && <LinksList links={items} />}
+          </Stack>
+        );
+      })}
+    </Stack>
+  );
+};
