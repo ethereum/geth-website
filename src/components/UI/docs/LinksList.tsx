5f7c7d03cae74699fe6768c167afe02eb04bf6c5
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-12-01 20:43:05 -0800

    add padding after sections in LinksList

--- src/components/UI/docs/LinksList.tsx
+++ src/components/UI/docs/LinksList.tsx
@@ -12,7 +12,7 @@ export const LinksList: FC<LinksListProps> = ({ links }) => (
   <Stack px={4}>
     {links.map(({ id, to, items }) => {
       return to ? (
-        <Stack key={id}>
+        <Stack key={id} pb={items ? 16 : 0}>
           <NextLink href={to} passHref key={id}>
             <Link>
               <Text textStyle='docs-nav-links' color={items ? 'primary' : 'body'}>
@@ -23,7 +23,7 @@ export const LinksList: FC<LinksListProps> = ({ links }) => (
           {items && <LinksList links={items} />}
         </Stack>
       ) : (
-        <Stack key={id}>
+        <Stack key={id} pb={16}>
           <Text textStyle='docs-nav-links' color={items ? 'primary' : 'body'}>
             {id}
           </Text>
