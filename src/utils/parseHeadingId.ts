dba847625fb88683c5bfa6df166d41cba6774af7
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-12-06 10:31:45 -0800

    move constant inside function
    make variable name all caps as a string constant

--- src/utils/parseHeadingId.ts
+++ src/utils/parseHeadingId.ts
@@ -1,11 +1,10 @@
 import { getKebabCaseFromName } from './';
 
-const check = '{#';
-
 export const parseHeadingId = (children: string[]) => {
+  const CHECK = '{#';
   const lastChild = children[children.length - 1];
-  const split = lastChild.split(check);
-  if (lastChild.includes(check)) {
+  const split = lastChild.split(CHECK);
+  if (lastChild.includes(CHECK)) {
     const headingId = split[split.length - 1].split('}')[0];
     const newChildren = [...children];
     newChildren[newChildren.length - 1] = split[0];
