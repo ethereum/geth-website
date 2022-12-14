1a23a8535a53e5ec572c76dde8113b77b1df2bc0
Nicolás Quiroz <nh.quiroz@gmail.com>
2022-12-14 17:52:22 -0300

    fix: typo & prettier

--- src/utils/chunkReleases.ts
+++ src/utils/chunkReleases.ts
@@ -2,10 +2,10 @@ import { ReleaseData } from './../types';
 
 export const chunkReleases = (arr: ReleaseData[], releaseVersions: string[]) => {
   // loop through release versions and chunk arr based on release version
-  const chunkedReleases = releaseVersions.map((releaseVersion) => {
-    return arr.filter((release) => {
-      return release.release.label.includes(releaseVersion)
-    })
-  }) 
-  return chunkedReleases
+  const chunkedReleases = releaseVersions.map(releaseVersion => {
+    return arr.filter(release => {
+      return release.release.label.includes(releaseVersion);
+    });
+  });
+  return chunkedReleases;
 };
--- src/utils/getSortedReleases.ts
+++ src/utils/getSortedReleases.ts
@@ -5,19 +5,26 @@ import { chunkReleases } from './chunkReleases';
 export const getSortedReleases = (releases: ReleaseData[], moreReleases: ReleaseData[] = []) => {
   const sortedReleasesByRelease = releases.concat(moreReleases).sort(compareReleasesFn);
 
-  const releaseVersions = sortedReleasesByRelease.map((release) => {
-    const spiltLabel = release.release.label.split(' ');
-    const releaseVersion = spiltLabel[spiltLabel.length - 1];
-    return releaseVersion
-  }).filter((version, i, self) => self.indexOf(version) == i)
+  const releaseVersions = sortedReleasesByRelease
+    .map(release => {
+      const splitLabel = release.release.label.split(' ');
+      const releaseVersion = splitLabel[splitLabel.length - 1];
+
+      return releaseVersion;
+    })
+    .filter((version, i, self) => self.indexOf(version) == i);
 
   const chunkedReleases = chunkReleases(sortedReleasesByRelease, releaseVersions);
-  
-  return chunkedReleases.map((chunk) => {
-    return chunk.sort((a, b) => {
-      return a.arch.localeCompare(b.arch);
-    }).sort((a, b) => {
-      return b.release.label.localeCompare(a.release.label);
-    });
-  }).flat();
+
+  return chunkedReleases
+    .map(chunk => {
+      return chunk
+        .sort((a, b) => {
+          return a.arch.localeCompare(b.arch);
+        })
+        .sort((a, b) => {
+          return b.release.label.localeCompare(a.release.label);
+        });
+    })
+    .flat();
 };
