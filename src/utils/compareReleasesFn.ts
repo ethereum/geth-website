550bb15628b9372fa40d18c63526b8311c510e75
Corwin Smith <cssmittys@gmail.com>
2022-12-14 22:39:19 -0700

    prettier

--- src/utils/compareReleasesFn.ts
+++ src/utils/compareReleasesFn.ts
@@ -1,18 +1,18 @@
 import { ReleaseData } from '../types';
 
 export const compareReleasesFn = (a: ReleaseData, b: ReleaseData) => {
-  const aPublished = new Date(a.published)
-  const bPublished = new Date(b.published)
-  const sameDate = aPublished.toDateString() === bPublished.toDateString()
-  const sameCommit = a.commit.label === b.commit.label
+  const aPublished = new Date(a.published);
+  const bPublished = new Date(b.published);
+  const sameDate = aPublished.toDateString() === bPublished.toDateString();
+  const sameCommit = a.commit.label === b.commit.label;
 
   if (sameDate && !sameCommit) {
-    return aPublished > bPublished ? -1 : 1
+    return aPublished > bPublished ? -1 : 1;
   }
 
   if (sameDate) {
-    return a.release.label.length - b.release.label.length
+    return a.release.label.length - b.release.label.length;
   }
 
-  return aPublished > bPublished ? -1 : 1
+  return aPublished > bPublished ? -1 : 1;
 };
