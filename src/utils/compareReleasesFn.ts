06bd831a6955f209279c1b82cfa3d654fdcec604
Corwin Smith <cssmittys@gmail.com>
2022-12-14 22:38:31 -0700

    fix sort

--- src/utils/compareReleasesFn.ts
+++ src/utils/compareReleasesFn.ts
@@ -3,9 +3,16 @@ import { ReleaseData } from '../types';
 export const compareReleasesFn = (a: ReleaseData, b: ReleaseData) => {
   const aPublished = new Date(a.published)
   const bPublished = new Date(b.published)
-  // If aPublished and bPublished are on the same day, sort by version
-  // else sort by date
-  return aPublished.toDateString() === bPublished.toDateString() ?
-    a.release.label.length - b.release.label.length :
-    aPublished > bPublished ? -1 : 1
+  const sameDate = aPublished.toDateString() === bPublished.toDateString()
+  const sameCommit = a.commit.label === b.commit.label
+
+  if (sameDate && !sameCommit) {
+    return aPublished > bPublished ? -1 : 1
+  }
+
+  if (sameDate) {
+    return a.release.label.length - b.release.label.length
+  }
+
+  return aPublished > bPublished ? -1 : 1
 };
