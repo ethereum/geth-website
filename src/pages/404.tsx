5c6f47e0613cf12d5741ef0afbae48923c7d1dae
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-12-15 21:11:33 -0800

    clean up styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -13,12 +13,30 @@ const Page404NotFound: NextPage = ({}) => {
       <PageMetadata title={METADATA.PAGE_404_TITLE} description={METADATA.PAGE_404_DESCRIPTION} />
 
       <main id='main-content'>
-        <Flex direction='column' alignItems='center' py={{ base: 12, md: 20 }} border="2px" borderColor="primary">
+        <Flex direction='column' alignItems='center' py={{ base: 16, md: 24 }} border="2px" borderColor="primary">
           <GopherHomeFront />
-          <Text fontSize={{ base: "8xl", md: "9xl" }} lineHeight='120%' fontFamily="heading" fontWeight="bold" textAlign="center">404</Text>
-          <Text fontSize="2xl" fontFamily="heading" fontWeight="bold" textAlign="center" mb={{ base: 6, md: 12 }}>page not found</Text>
+          <Text
+            fontSize={{ base: "8xl", md: "9xl" }}
+            lineHeight='120%'
+            fontFamily='heading'
+            textAlign='center'
+            letterSpacing='0.05em'
+            fontWeight='500'
+          >
+            404
+          </Text>
+          <Text
+            fontSize={{ base: 'xl', md: '2xl'}}
+            fontFamily='heading'
+            fontWeight='700'
+            textAlign='center'
+            mb={{ base: 8, md: 12 }}
+            letterSpacing='0.05em'
+          >
+            page not found
+          </Text>
           <NextLink href='/' passHref legacyBehavior>
-            <Button variant='primary' as='a' mb={1} data-group>
+            <Button variant='primary' as='a' data-group>
               <Text textStyle='homepage-primary-label'>Go to homepage</Text>
             </Button>
           </NextLink>
